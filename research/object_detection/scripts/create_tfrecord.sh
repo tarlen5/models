e3cdd025ec47720ae1a9c1a6f34891f3da3c6f48
root <ralasic1@gmail.com>
2020-07-31 11:31:10 +0000

    Changes to scripts #2

--- research/object_detection/scripts/create_tfrecord.sh
+++ research/object_detection/scripts/create_tfrecord.sh
@@ -7,7 +7,7 @@ VAL_ANNOTATIONS_FILE=$(TRAIN_ANNOTATIONS_FILE)
 TESTDEV_ANNOTATIONS_FILE=$(TRAIN_ANNOTATIONS_FILE)
 OUTPUT_DIR="./object_detection/datasets/val2017/tfrecords/"
 
-python create_coco_tf_record.py --logtostderr \
+python ./object_detection/dataset_tools/create_coco_tf_record.py --logtostderr \
   --train_image_dir="${TRAIN_IMAGE_DIR}" \
   --val_image_dir="${VAL_IMAGE_DIR}" \
   --test_image_dir="${TEST_IMAGE_DIR}" \
