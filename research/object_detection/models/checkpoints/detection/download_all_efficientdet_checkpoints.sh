bce0e0a9941f3fcd956e2765c28b379238daf7fd
root <ralasic1@gmail.com>
2020-07-31 10:15:06 +0000

    Added script to download EfficientDet checkpoints

---
+++ research/object_detection/models/checkpoints/detection/download_all_efficientdet_checkpoints.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in {0..7}
+do
+  #echo "$i"
+  curl "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d${i}_coco17_tpu-32.tar.gz" -o "efficientdet_d${i}_coco17_tpu-32.tar.gz"
+  tar -xzf "efficientdet_d${i}_coco17_tpu-32.tar.gz"
+done
